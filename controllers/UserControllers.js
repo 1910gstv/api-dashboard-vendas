bf52cc945975af1f83683f49010848b061c31fe4
Gustavo <gustavo.hpaula@outlook.com>
2023-10-28 20:24:32 -0300

    API com funcao de login já implementada

---
+++ controllers/UserControllers.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const bodyParser = require("body-parser");
+
+// router.get("/users", (req, res) => {
+//   User.findAll({ raw: true }).then((users) => {
+//     res.json(users);
+//   });
+// });
+
+// router.post("/authenticate", (req, res) => {
+//   //Autenticar com JWT
+//   var login = req.body.login;
+//   var password = req.body.senha;
+//   var autenticado;
+
+//   User.findOne({
+//     where: {
+//       login: login,
+//     },
+//   }).then(user => {
+//     if(user != undefined){
+//         if(user.senha == password) {
+//             autenticado = true;
+//             res.json({user: user , autenticado: autenticado})
+//         } else {
+//             autenticado = false;
+//             res.json({user: user, autenticado: autenticado})
+//         } 
+//     } else {
+//         autenticado = false;
+//         res.json({user: user, autenticado: autenticado})
+//     }
+//   }).catch((err) => {
+//     res.send(err)
+//   })
+// });
+
+
+    function authenticate (req, res) {
+        var login = req.body.login;
+       var password = req.body.senha;
+       var autenticado;
+    
+       User.findOne({
+         where: {
+           login: login,
+         },
+       }).then(user => {
+         if(user != undefined){
+             if(user.senha == password) {
+                 autenticado = true;
+                 res.json({user: user , autenticado: autenticado})
+             } else {
+                 autenticado = false;
+                 res.json({user: user, autenticado: autenticado})
+             } 
+         } else {
+             autenticado = false;
+             res.json({user: user, autenticado: autenticado})
+         }
+       }).catch((err) => {
+         res.send(err)
+    })
+  }
+
+
+
+
+module.exports = { authenticate };
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const connection = require("./database/database");
+const cors = require('cors');
+
+const userController = require('./routes/usersRoutes')
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/', userController)
+
+app.use(session({
+    secret: "paocomcafe",
+    cookie: {
+        maxAge: 30000000000
+    }
+}))
+
+connection.authenticate().then(() => {
+    console.log('Conexão feita com sucesso');
+}).catch((err) => {
+    console.log(err);
+})
+
+app.listen(8081, () => {
+    console.log('API RODANDO')
+})
